040493a634f73592626075957f14d032305b6357
AlexMurashko <alexeymurashko1995@gmail.com>
2026-03-23 15:24:20 +0100

    Update Task 1 Logic

--- tasks_2303.py
+++ tasks_2303.py
@@ -3,10 +3,11 @@ print('--- Task 1: Space ---')
 money = int(input('Enter the amount of chattles: '))
 
 CR = float(money / 2200)
-space_ship = int(CR * 1 / 0.5)
+ship_price = 0.5
+space_ship = int(CR / ship_price)
+
 print(f'You have {CR} CR')
 print(f'You can buy: {space_ship} space ships')
-
 print('*' * 20)
 
 
