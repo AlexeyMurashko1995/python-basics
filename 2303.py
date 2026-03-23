3b122dcc9a4bcfb8b7cc060f5179490f8b01b5c2
AlexMurashko <alexeymurashko1995@gmail.com>
2026-03-23 15:09:53 +0100

    Added Space Rangers Task

---
+++ 2303.py
@@ -0,0 +1,4 @@
+money = int(input('Enter the amount of chattles: '))
+CR = float(money / 2200)
+space_ship = int(CR * 1 / 0.5)
+print(f'You have {CR} CR\nYou can buy: {space_ship} space ships')
---
+++ tasks 2303.py
@@ -0,0 +1,11 @@
+#--- TASK 1: SPACE RANGERS ---
+print('Task 1: Space ')
+money = int(input('Enter the amount of chattles: '))
+
+CR = float(money / 2200)
+space_ship = int(CR * 1 / 0.5)
+print(f'You have {CR} CR')
+print(f'You can buy: {space_ship} space ships')
+print('*' * 10)
+
+
