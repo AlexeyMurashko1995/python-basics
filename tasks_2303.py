0ef295bc257a87e77710e65c2f41eb7e2490b9a2
AlexMurashko <alexeymurashko1995@gmail.com>
2026-03-23 21:08:49 +0100

    Finish Task 3: integrated piece adjustment logic

--- tasks_2303.py
+++ tasks_2303.py
@@ -1,4 +1,4 @@
-#--- TASK 1: SPACE RANGERS ---
+# #--- TASK 1: SPACE RANGERS ---
 print('--- Task 1: Space ---')
 money = int(input('Enter the amount of chattles: '))
 
@@ -16,14 +16,22 @@ print(f'Enter the piece location: ')
 x = float(input('Enter a number horizontally: '))
 y = float(input('Enter a number vertically: '))
 
+
+
 x_square = int(x * 10)
+x_ideal = (x_square * 0.1) + 0.05
+dif_x = round(x_ideal - x, 3)
 y_square = int(y * 10)
+y_ideal = (y_square * 0.1) + 0.05
+dif_y = round(y_ideal - y, 3)
 
 if x_square > 7 or y_square > 7 or x_square < 0 or y_square < 0:
     print('This coordinate does not exist! ')
 else:
     print(f'The piece is at ({x_square}, {y_square})')
+    print(f'Adjust the piece position to ({dif_x}, {dif_y})')
 print('*' * 20)
+
 
 
 
