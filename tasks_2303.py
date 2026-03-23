1ef06e1679d0e49d3cd692f7579c9d4d675f859b
AlexMurashko <alexeymurashko1995@gmail.com>
2026-03-23 15:56:28 +0100

    Added Task 2: Chessboard

--- tasks_2303.py
+++ tasks_2303.py
@@ -1,14 +1,28 @@
-#--- TASK 1: SPACE RANGERS ---
-print('--- Task 1: Space ---')
-money = int(input('Enter the amount of chattles: '))
+# #--- TASK 1: SPACE RANGERS ---
+# print('--- Task 1: Space ---')
+# money = int(input('Enter the amount of chattles: '))
 
-CR = float(money / 2200)
-ship_price = 0.5
-space_ship = int(CR / ship_price)
+# CR = float(money / 2200)
+# ship_price = 0.5
+# space_ship = int(CR / ship_price)
 
-print(f'You have {CR} CR')
-print(f'You can buy: {space_ship} space ships')
-print('*' * 20)
+# print(f'You have {CR} CR')
+# print(f'You can buy: {space_ship} space ships')
+# print('*' * 20)
+
+#--- TASK 2: CHESSBOARD ---
+print('--- Task 2: Chessboard ---')
+print(f'Enter the piece location: ')
+x = float(input('Enter a number horizontally: '))
+y = float(input('Enter a number vertically: '))
+
+x_square = int(x * 10)
+y_square = int(y * 10)
+
+if x_square > 8 or y_square > 8:
+    print('This coordinate does not exist! ')
+else:
+    print(f'The piece is at ({x_square}, {y_square})')
 
 
 
