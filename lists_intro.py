ef1230d41ee215cc958edfa7014e7cb003830dfb
AlexMurashko <alexeymurashko1995@gmail.com>
2026-04-06 18:15:10 +0200

    feat: add task 3

--- lists_intro.py
+++ lists_intro.py
@@ -17,3 +17,13 @@ print(f'The length of the list: {len(my_numbers)}')
 print(f'The last: {my_numbers[10]}')
 
 print('*' * 30)
+
+# --- PROGRAM 3: STRING EXPLOSION ---
+print('--- Task 3: String Explosion ---')
+
+s = 'Python2026'
+my_list = list(s)
+
+print(f'My list by char: {my_list}\nIndex #6 element: {my_list[6]}')
+
+print('*' * 30)
