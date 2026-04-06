5d765ce5e99d90024ab8a4c7b915de48d3e5ee53
AlexMurashko <alexeymurashko1995@gmail.com>
2026-04-06 18:07:55 +0200

    feat: add task 2

--- lists_intro.py
+++ lists_intro.py
@@ -7,3 +7,13 @@ print(f'The third element: {shopping_list[2]}')
 print(f'All items: {shopping_list}')
 
 print('*' * 30)
+
+# --- PROGRAM 2: RANGE MAGIC ---
+print('--- Task 2: Range Magic ---')
+
+my_numbers = list(range(1, 22, 2))
+
+print(f'The length of the list: {len(my_numbers)}')
+print(f'The last: {my_numbers[10]}')
+
+print('*' * 30)
