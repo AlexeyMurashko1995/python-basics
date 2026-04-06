3fbefd809c831ec3ed880d255306dfe46ff35815
AlexMurashko <alexeymurashko1995@gmail.com>
2026-04-06 17:59:27 +0200

    feat: initialize lists_intro and complete task 1

---
+++ lists_intro.py
@@ -0,0 +1,9 @@
+# --- PROGRAM 1: THE WARSAW SHOPPING LIST ---
+print('--- Task 1: The Warsaw Shopping List ---')
+
+shopping_list = ['bread', 'meat', 'milk', 'fish', 'ice-cream']
+
+print(f'The third element: {shopping_list[2]}')
+print(f'All items: {shopping_list}')
+
+print('*' * 30)
