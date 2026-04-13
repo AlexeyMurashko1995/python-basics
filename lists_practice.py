64641f7b3871d676a469248db0ffc2cf6639110b
AlexMurashko <alexeymurashko1995@gmail.com>
2026-04-13 11:12:41 +0200

    add file list_practice and task 1: apartment indexing

---
+++ lists_practice.py
@@ -0,0 +1,15 @@
+# --- PROGRAM 1: APARTMENT INDEXING ---
+print('--- Task 1: Apartment Indexing ---')
+
+apartment_1 = ["Mokotow", 45, 600000]
+apartment_2 = ["Wola", 38, 520000]
+apartment_3 = ["Praga", 50, 480000]
+
+apartments = [
+    apartment_1,
+    apartment_2,
+    apartment_3
+]
+
+print(apartments[1][2])
+print('*' * 30)
