715fe1582e4eda54592e4c6e25b6199ef7b56f17
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-06 15:54:55 +0200

    feat: evaluate model with train_test_split and check R2 score

--- pandas_grouping_practice.py
+++ pandas_grouping_practice.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+
+pd.options.display.float_format = '{:.2f}'.format
 
 # Load the dataset
 df = pd.read_csv('apartments_lab.csv', encoding='utf-8-sig')
@@ -36,11 +40,24 @@ plt.show()
 #Starting work with scikit-learn
 X = df[['area']]
 y = df['price']
-model = LinearRegression()
-model.fit(X, y)
 
-print(f'Price per sqm: {model.coef_[0]}')
-print(f'Intercept: {model.intercept_}')
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+score = model.score(X_test, y_test)
+
+print(f'Model accuracy (R2 score): {score}')
+
+y_pred = model.predict(X_test)
+
+comparison = pd.DataFrame({
+    'Actual': y_test.values[:5],
+    'Predicted': y_pred[:5]
+})
+
+print(comparison)
 
 
 # grouped = df.groupby('area_name')
