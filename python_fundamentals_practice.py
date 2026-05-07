f40c22e5b4fefa0bacf98ff563e0317a3a4fc8f6
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-07 20:58:43 +0200

    feat: add task #2: credit card masking

--- python_fundamentals_practice.py
+++ python_fundamentals_practice.py
@@ -1,18 +1,27 @@
-# --- TASK 1: NUMBER REVERSAL ---
-print('--- Task 1: Number Reversal ---')
+# # --- TASK 1: NUMBER REVERSAL ---
+# print('--- Task 1: Number Reversal ---')
 
-user_number = int(input())
-user_number = str(user_number)
+# user_number = int(input())
+# user_number = str(user_number)
 
-if len(user_number) == 6:
-    prefix = user_number[0:1:]
-    reverse = user_number[-1:0:-1]
-    result = int(prefix + reverse)
+# if len(user_number) == 6:
+#     prefix = user_number[0:1:]
+#     reverse = user_number[-1:0:-1]
+#     result = int(prefix + reverse)
 
-else:
-    result = int(user_number[::-1])
+# else:
+#     result = int(user_number[::-1])
 
-print(result)
+# print(result)
+# print('*' * 30)
+
+# --- TASK 2: CREDIT CARD MASKING ---
+print('--- Task 2: Credit Card Masking ---')
+
+user_card = int(input())
+user_card = str(user_card)
+
+user_card_feat = '*' * 12 + user_card[12:]
+
+print(user_card_feat)
 print('*' * 30)
-
-
