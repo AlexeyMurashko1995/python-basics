7814567ed23b83a9c2a77846b04968d64d703bed
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-11 09:09:35 +0200

    feat: add tasl #6: josephus problem

--- python_fundamentals_practice.py
+++ python_fundamentals_practice.py
@@ -1,71 +1,87 @@
-# --- TASK 1: NUMBER REVERSAL ---
-print('--- Task 1: Number Reversal ---')
+# # --- TASK 1: NUMBER REVERSAL ---
+# print('--- Task 1: Number Reversal ---')
 
-user_number = int(input())
-user_number = str(user_number)
+# user_number = int(input())
+# user_number = str(user_number)
 
-if len(user_number) == 6:
-    prefix = user_number[0:1:]
-    reverse = user_number[-1:0:-1]
-    result = int(prefix + reverse)
+# if len(user_number) == 6:
+#     prefix = user_number[0:1:]
+#     reverse = user_number[-1:0:-1]
+#     result = int(prefix + reverse)
 
-else:
-    result = int(user_number[::-1])
+# else:
+#     result = int(user_number[::-1])
 
-print(result)
-print('*' * 30)
+# print(result)
+# print('*' * 30)
 
-# --- TASK 2: CREDIT CARD MASKING ---
-print('--- Task 2: Credit Card Masking ---')
+# # --- TASK 2: CREDIT CARD MASKING ---
+# print('--- Task 2: Credit Card Masking ---')
 
-user_card = int(input())
-user_card = str(user_card)
+# user_card = int(input())
+# user_card = str(user_card)
 
-user_card_feat = '*' * 12 + user_card[12:]
+# user_card_feat = '*' * 12 + user_card[12:]
 
-print(user_card_feat)
-print('*' * 30)
+# print(user_card_feat)
+# print('*' * 30)
 
-# --- TASK 3: TASK 3: REVERSED NAME BRANDING ---
-print('--- Task 3: Reverse Name Branding ---')
+# # --- TASK 3: TASK 3: REVERSED NAME BRANDING ---
+# print('--- Task 3: Reverse Name Branding ---')
 
-user_input = input().lower()
+# user_input = input().lower()
 
-first_letter = user_input[:1].capitalize()
-reverse = user_input [-1:0:-1]
+# first_letter = user_input[:1].capitalize()
+# reverse = user_input [-1:0:-1]
 
-print(first_letter + reverse)
-print('*' * 30)
+# print(first_letter + reverse)
+# print('*' * 30)
 
-# --- TASK 4: DYNAMIC STRING SPLIT & SWAP ---
-print('--- Task 4: Dynamic String Split & Swap ---')
+# # --- TASK 4: DYNAMIC STRING SPLIT & SWAP ---
+# print('--- Task 4: Dynamic String Split & Swap ---')
 
-user_input = input()
+# user_input = input()
 
-if len(user_input) % 2 == 0:
-    first_half = user_input[len(user_input) // 2:]
-    second_half = user_input[:len(user_input) // 2]
-    filtered_output = first_half + second_half
-else:
-    filtered_output = user_input
+# if len(user_input) % 2 == 0:
+#     first_half = user_input[len(user_input) // 2:]
+#     second_half = user_input[:len(user_input) // 2]
+#     filtered_output = first_half + second_half
+# else:
+#     filtered_output = user_input
 
-print(filtered_output)
-print('*' * 30)
+# print(filtered_output)
+# print('*' * 30)
 
-# --- TASK 5: STANDARD AMERICAN CONVENTION ---
-print('--- Task 5: Standard American Convention ---')
+# # --- TASK 5: STANDARD AMERICAN CONVENTION ---
+# print('--- Task 5: Standard American Convention ---')
 
-user_input = int(input())
-user_input = str(user_input)
+# user_input = int(input())
+# user_input = str(user_input)
 
-result = []
+# result = []
 
-while len(user_input) > 3:
-    result.append(user_input[-3:])
-    user_input = user_input[:-3]
+# while len(user_input) > 3:
+#     result.append(user_input[-3:])
+#     user_input = user_input[:-3]
 
-result.append(user_input)
-result.reverse()
+# result.append(user_input)
+# result.reverse()
 
-print(','.join(result))
-print('*' * 30)
+# print(','.join(result))
+# print('*' * 30)
+
+# --- TASK 6: JOSEPHUS PROBLEM ---
+print('--- Task 6: Josephus Problem ---')
+
+n = int(input(''))
+k = int(input(''))
+
+people_list = list(range(1,n + 1))
+index = 0
+
+while len(people_list) > 1:
+    i = (index + k - 1) % len(people_list)
+    people_list.pop(i)
+    index = i
+
+print(people_list[0])
