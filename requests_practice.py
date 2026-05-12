3607e466eaa26f3b84fa95438b933ef9042c50f7
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-12 12:07:17 +0200

    feat: complete API practice with parameters, headers and security

--- requests_practice.py
+++ requests_practice.py
@@ -1,17 +1,20 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-# # Task 1. Practising with request GET (1 param)
-# url = 'https://api.agify.io/'
 
-# params = {'name': 'Alex'}
+# Task 1. Practising with a GET request (1 parameter)
+url = 'https://api.agify.io/'
 
-# response = requests.get(url, params=params)
+params = {'name': 'Alex'}
 
-# if response.ok:
-#     data = response.json()
-#     print(f'Age: {data['age']}')
+response = requests.get(url, params=params)
 
-# Task 2. Practising with API using several params
+if response.ok:
+    data = response.json()
+    print(f'Age: {data['age']}')
+
+# Task 2. Practising with APIs using multiple parameters
 
 url = 'https://api.open-meteo.com/v1/forecast'
 
@@ -26,3 +29,17 @@ response = requests.get(url, params=params)
 if response.ok:
     data = response.json()
     print(data['current_weather']['temperature'])
+
+# Task 3. Working with API keys and headers (Secure connection)
+
+load_dotenv()
+api_key = os.getenv('API_NINJA_KEY')
+
+url = 'https://api.api-ninjas.com/v1/facts'
+headers = {'X-Api-Key': api_key}
+
+response = requests.get(url, headers=headers)
+
+if response.ok:
+    data = response.json()
+    print(f"Interesting fact: {data[0]['fact']}")
