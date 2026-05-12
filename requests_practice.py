bfa6ac2853d3fde41277a322110830e12be74c4e
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-12 10:55:00 +0200

    feat: implement weather API request with multiple parameters and nested data parsing

--- requests_practice.py
+++ requests_practice.py
@@ -1,11 +1,28 @@
 import requests
 
-url = 'https://api.agify.io/'
+# # Task 1. Practising with request GET (1 param)
+# url = 'https://api.agify.io/'
 
-params = {'name': 'Alex'}
+# params = {'name': 'Alex'}
+
+# response = requests.get(url, params=params)
+
+# if response.ok:
+#     data = response.json()
+#     print(f'Age: {data['age']}')
+
+# Task 2. Practising with API using several params
+
+url = 'https://api.open-meteo.com/v1/forecast'
+
+params = {
+    'latitude' : 52.23,
+    'longitude' : 21.01,
+    'current_weather' : True
+}
 
 response = requests.get(url, params=params)
 
 if response.ok:
     data = response.json()
-    print(f'Age: {data['age']}')
+    print(data['current_weather']['temperature'])
