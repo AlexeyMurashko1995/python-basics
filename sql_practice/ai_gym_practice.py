e7c03d3a0063af628f012a4b5b18a84f8204353d
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-17 18:58:50 +0200

    refactor: secure API key using python-dotenv with root folder search

--- sql_practice/ai_gym_practice.py
+++ sql_practice/ai_gym_practice.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import sqlite3
 from google import genai
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 conn = sqlite3.connect('gym.db')
 
@@ -13,3 +17,11 @@ conn.close()
 df_text = all_clients_df.to_string()
 
 print(df_text)
+
+prompt = f"""Business-analytic: {df_text}"""
+
+client = genai.Client()
+
+response = client.models.generate_content(model='gemini-2.5-flash', contents=prompt)
+
+print(response.text)
