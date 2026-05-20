cd7767ed8282e2104ab2797601d3a6730b4ef5c3
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-20 21:46:01 +0200

    feat: instantiate SQLModel objects for cargo tracking

--- orm_practice.py
+++ orm_practice.py
@@ -6,3 +6,14 @@ class Cargo(SQLModel, table=True):
     destination: str
     weight: int
     status: str
+
+cargo_one = Cargo(
+    id_tracking='TRK-101', destination='Warsaw', weight=450, status='In Transit'
+)
+
+cargo_two = Cargo(
+    id_tracking='TRK-202', destination='Wroclaw', weight=890, status='Delivered'
+)
+
+print(f'Cargo {cargo_one.id_tracking} has weight {cargo_one.weight} kg')
+print(f'Cargo {cargo_two.id_tracking} has weight {cargo_two.weight} kg')
