e7abb1c80f396eff770c0d86363e309c40c90b1c
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-21 17:04:59 +0200

    refactor: protect execution logic with __name__ == '__main__' conditional guard

--- orm_practice.py
+++ orm_practice.py
@@ -1,7 +1,5 @@
 from sqlmodel import Field, SQLModel, create_engine, Session
 
-
-
 class Cargo(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     id_tracking: str
@@ -9,26 +7,25 @@ class Cargo(SQLModel, table=True):
     weight: int
     status: str
 
-cargo_one = Cargo(
-    id_tracking='TRK-101', destination='Warsaw', weight=450, status='In Transit'
-)
-
-cargo_two = Cargo(
-    id_tracking='TRK-202', destination='Wroclaw', weight=890, status='Delivered'
-)
-
-print(f'Cargo {cargo_one.id_tracking} has weight {cargo_one.weight} kg')
-print(f'Cargo {cargo_two.id_tracking} has weight {cargo_two.weight} kg')
-
 sqlite_file_name = 'database.db'
 sqlite_url = f'sqlite:///{sqlite_file_name}'
-
 engine = create_engine(sqlite_url)
 
-SQLModel.metadata.create_all(engine)
+if __name__ == "__main__":
 
-with Session(engine) as session:
-    session.add(cargo_one)
-    session.add(cargo_two)
+    cargo_one = Cargo(
+        id_tracking='TRK-101', destination='Warsaw', weight=450, status='In Transit'
+    )
+    cargo_two = Cargo(
+        id_tracking='TRK-202', destination='Wroclaw', weight=890, status='Delivered'
+    )
 
-    session.commit()
+    print(f'Cargo {cargo_one.id_tracking} has weight {cargo_one.weight} kg')
+    print(f'Cargo {cargo_two.id_tracking} has weight {cargo_two.weight} kg')
+
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        session.add(cargo_one)
+        session.add(cargo_two)
+        session.commit()
