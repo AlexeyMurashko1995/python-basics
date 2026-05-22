186806ab144f877dd90262b5b998587a5a28eb2e
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-22 16:41:05 +0200

    style: reorder Warehouse model to top according to PEP 8 and fix output grammar

--- database_practice/orm_warehouses.py
+++ database_practice/orm_warehouses.py
@@ -11,8 +11,8 @@ url = f'sqlite:///{sql_file_name}'
 
 engine = create_engine(url)
 
-if __name__ == '__main__':
 
+def init_db_and_add_warehouses():
     warehouse_one = Warehouse(name='Janki', city='Warsaw', capacity_tons=550)
     warehouse_two = Warehouse(name='Gdanski', city='Gdansk', capacity_tons=1100)
 
@@ -24,20 +24,34 @@ if __name__ == '__main__':
 
         session.commit()
 
-        second_warehouse = session.get(Warehouse, 2)
-        if second_warehouse:
-            second_warehouse.capacity_tons = 1200
+
+def update_warehouse_capacity(warehouse_id: int, new_capacity: int):
+    with Session(engine) as session:
+        target_warehouse = session.get(Warehouse, warehouse_id)
+        if target_warehouse:
+            target_warehouse.capacity_tons = new_capacity
             session.commit()
 
-        first_warehouse = session.get(Warehouse, 1)
-        if first_warehouse:
-            session.delete(first_warehouse)
+
+def delete_warehouse(warehouse_id: int):
+    with Session(engine) as session:
+        target_warehouse = session.get(Warehouse, warehouse_id)
+        if target_warehouse:
+            session.delete(target_warehouse)
             session.commit()
 
+
+def show_all_warehouses():
+    with Session(engine) as session:
         query = select(Warehouse)
         result = session.exec(query)
 
         for warehouse in result:
-            print(f'Name: {warehouse.name}; City: {warehouse.city}; Capacity tons: {warehouse.capacity_tons}')
+            print(f'Warehouse name: {warehouse.name} is located in {warehouse.city}; capacity tons: {warehouse.capacity_tons}')
 
 
+if __name__ == '__main__':
+    init_db_and_add_warehouses()
+    update_warehouse_capacity(2, 1200)
+    delete_warehouse(1)
+    show_all_warehouses()
