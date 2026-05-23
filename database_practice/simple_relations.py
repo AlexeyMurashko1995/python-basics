e569b226e580d02cd17454bdd2d88cfb7b8a3e05
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-23 21:21:40 +0200

    feat: implement flat database schema and manual data seeding for users and posts

--- database_practice/simple_relations.py
+++ database_practice/simple_relations.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, Field, Session, select, Relationship
+from sqlmodel import SQLModel, Field, create_engine, Session, select, Relationship
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -8,3 +8,31 @@ class Post(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     title: str
     user_id: int = Field(foreign_key='user.id')
+
+sql_file_name = 'simple_db.db'
+url = f'sqlite:///{sql_file_name}'
+
+engine = create_engine(url)
+
+
+def init_db_and_fill():
+    user_one = User(username='Alex')
+
+    post_one = Post(title='Relax', user_id=1)
+    post_two = Post(title='Sleep', user_id=1)
+
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        session.add(user_one)
+
+        session.add(post_one)
+        session.add(post_two)
+
+        session.commit()
+
+
+if __name__ == '__main__':
+    init_db_and_fill()
+
+
