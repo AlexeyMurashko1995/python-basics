11144b552febd9909e892a926bb9cd69c30c2f22
AlexMurashko <alexeymurashko1995@gmail.com>
2026-05-24 22:26:39 +0200

    feat: add create_music_data function to insert data using relationships

--- database_practice/music_models.py
+++ database_practice/music_models.py
@@ -22,5 +22,19 @@ def init_db():
     print('Database created successfully')
 
 
+def create_music_data():
+    with Session(engine) as session:
+        artist_one = Artist(name='Eminem')
+        track_one = Track(title='Loose Yourself')
+
+        track_one.artist = artist_one
+
+        session.add(artist_one)
+        session.add(track_one)
+
+        session.commit()
+
+
 if __name__ == '__main__':
     init_db()
+    create_music_data()
