3da9092ecac3f09cd9b3a980c1b70a4d1fba5798
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-07 19:52:17 +0200

    feat: add task #2

--- advanced_topics/args_kwargs_practice.py
+++ advanced_topics/args_kwargs_practice.py
@@ -2,9 +2,20 @@
 # Task 1
 # --------------------------------
 
+# def find_highest_score(*args):
+#     return max(*args)
 
-def find_highest_score(*args):
-    return max(*args)
+
+# print(f'[Analytics] Maximum score for the exam: {find_highest_score(85, 92, 78, 99, 64)}')
 
 
-print(f'[Analytics] Maximum score for the exam: {find_highest_score(85, 92, 78, 99, 64)}')
+# --------------------------------
+# Task 2
+# --------------------------------
+
+def print_user_profile(**kwargs):
+    for key, value in kwargs.items():
+        print(f'-{key}: {value}')
+
+
+print_user_profile(username="alex_dev", age=30, email="alex@example.com")
