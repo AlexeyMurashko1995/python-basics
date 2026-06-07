97dc3439fbc3cb10b9e95335d95b92b0f1295f5b
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-07 19:41:47 +0200

    feat: implement highest score finder using *args

---
+++ advanced_topics/args_kwargs_practice.py
@@ -0,0 +1,10 @@
+# --------------------------------
+# Task 1
+# --------------------------------
+
+
+def find_highest_score(*args):
+    return max(*args)
+
+
+print(f'[Analytics] Maximum score for the exam: {find_highest_score(85, 92, 78, 99, 64)}')
