2039c4622a3b341ced4648ad1f0da862cc0c6cce
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-08 21:14:32 +0200

    feat: add new file with generators_practice

---
+++ advanced_topics/generators_practice.py
@@ -0,0 +1,13 @@
+# -------------------------------
+# Task 1.
+# -------------------------------
+
+
+def count_to_three():
+    yield '1'
+    yield '2'
+    yield '3'
+
+
+result = count_to_three()
+print(result)
