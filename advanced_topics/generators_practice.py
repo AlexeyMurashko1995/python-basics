a5a929e6f2b5afa0a2ff56a764812ba3db0d09d4
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-08 21:19:59 +0200

    feat: use next() to advance count_to_three generator

--- advanced_topics/generators_practice.py
+++ advanced_topics/generators_practice.py
@@ -3,11 +3,27 @@
 # -------------------------------
 
 
+# def count_to_three():
+#     yield '1'
+#     yield '2'
+#     yield '3'
+
+
+# result = count_to_three()
+# print(result)
+
+# -------------------------------
+# Task 2.
+# -------------------------------
+
+
 def count_to_three():
     yield '1'
     yield '2'
     yield '3'
 
-
 result = count_to_three()
-print(result)
+
+print(next(result))
+print(next(result))
+print(next(result))
