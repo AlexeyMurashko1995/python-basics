8b0b7353883ab014e7d654247e48ecc3a60b0ba7
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-08 21:33:25 +0200

    feat: implement custom_range generator with internal loop

--- advanced_topics/generators_practice.py
+++ advanced_topics/generators_practice.py
@@ -32,13 +32,27 @@
 # Task 3.
 # -------------------------------
 
-def count_to_three():
-    yield '1'
-    yield '2'
-    yield '3'
+# def count_to_three():
+#     yield '1'
+#     yield '2'
+#     yield '3'
 
 
-result = count_to_three()
+# result = count_to_three()
 
-for string in result:
-    print(string)
+# for string in result:
+#     print(string)
+
+# -------------------------------
+# Task 4.
+# -------------------------------
+
+def custom_range(n):
+    for number in range(1, n + 1):
+        yield number
+
+
+result = custom_range(5)
+
+for value in result:
+    print(value)
