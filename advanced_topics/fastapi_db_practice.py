8e5f641aec6e17e4705d75f468bf53879152c886
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-12 21:59:44 +0200

    feat: add Bike SQLModel table with id auto-generation and validation

--- advanced_topics/fastapi_db_practice.py
+++ advanced_topics/fastapi_db_practice.py
@@ -1,8 +1,16 @@
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel, Session, create_engine, Field
 from fastapi import FastAPI
 
 
 app = FastAPI()
+
+
+class Bike(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    model: str = Field(min_length=2)
+    type: str
+    price: int = Field(gt=0)
+
 
 sqlite_filename = 'database.db'
 url = f'sqlite:///{sqlite_filename}'
