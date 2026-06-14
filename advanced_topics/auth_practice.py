031068848bb69d3bd2076828b0c6252403e6d8ef
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-14 20:31:45 +0200

    feat: implement token verification logic

--- advanced_topics/auth_practice.py
+++ advanced_topics/auth_practice.py
@@ -5,5 +5,11 @@ ALGORITHM = 'HS256'
 
 
 def create_token(user_id: int):
-    payload = {'sub': str(user_id)}
+    payload = {'sub': str(user_id)}Ы
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def verify_token(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    id = payload['sub']
+    return int(id)
