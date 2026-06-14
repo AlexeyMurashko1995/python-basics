7c7e33d2ada7ee31fd6f28736df4678d72439abd
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-14 20:03:13 +0200

    feat: implement JWT token generation function

--- advanced_topics/auth_practice.py
+++ advanced_topics/auth_practice.py
@@ -3,3 +3,7 @@ import jwt
 SECRET_KEY = 'my_secret_key'
 ALGORITHM = 'HS256'
 
+
+def create_token(user_id: int):
+    payload = {'sub': str(user_id)}
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
