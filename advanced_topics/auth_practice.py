bbd0e5c037ce248521eb6baa475d4b0a7cbac160
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-14 21:15:25 +0200

    feat: complete rewrite of JWT auth from memory

--- advanced_topics/auth_practice.py
+++ advanced_topics/auth_practice.py
@@ -5,11 +5,16 @@ ALGORITHM = 'HS256'
 
 
 def create_token(user_id: int):
-    payload = {'sub': str(user_id)}Ы
+    payload = {'sub': str(user_id)}
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def verify_token(token: str):
+def verify_id(token: str):
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    id = payload['sub']
-    return int(id)
+    return int(payload['sub'])
+
+my_token = create_token(5)
+print(f'My token: {my_token}')
+
+my_id = verify_id(my_token)
+print(f'My ID: {my_id}')
