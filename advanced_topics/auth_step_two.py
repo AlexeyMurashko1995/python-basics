fc1daff1bf1d8a1834726c41fd410721f5fd5c3e
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-16 20:46:23 +0200

    feat: add OAuth2PasswordBearer dependency to verify_token

--- advanced_topics/auth_step_two.py
+++ advanced_topics/auth_step_two.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 import jwt
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = 'my_secret_key'
 ALGORITHM = 'HS256'
@@ -12,7 +15,7 @@ def create_token(user_id: int):
     return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def verify_token(token: str):
+def verify_token(token: str = Depends(oauth2_scheme)):
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     return int(payload['sub'])
 
