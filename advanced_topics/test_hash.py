4f156a5cc5f28f0667d7fb9c82c43212fc8c7620
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-17 21:16:23 +0200

    feat: complete password verification logic using bcrypt

--- advanced_topics/test_hash.py
+++ advanced_topics/test_hash.py
@@ -5,5 +5,13 @@ def get_password_hash(password: str):
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     password_hash = bcrypt.hashpw(password_bytes, salt)
-    password_string = password_hash.decode('utf-8')
-    return password_string
+    hash_string = password_hash.decode('utf-8')
+    return hash_string
+
+
+def verify_password(password: str, hash_string: str):
+    password_bytes = password.encode('utf-8')
+    hash_bytes = hash_string.encode('utf-8')
+    verification = bcrypt.checkpw(password_bytes, hash_bytes)
+    return verification
+
