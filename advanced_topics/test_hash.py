07efd02d986eb7eb5017d07b8adc92e07d176c9a
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-17 21:02:27 +0200

    feat: successfully rebuild password hashing from memory

--- advanced_topics/test_hash.py
+++ advanced_topics/test_hash.py
@@ -4,7 +4,6 @@ import bcrypt
 def get_password_hash(password: str):
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    password_hashed = bcrypt.hashpw(password_bytes, salt)
-    clean_password = password_hashed.decode('utf-8')
-    return clean_password
-
+    password_hash = bcrypt.hashpw(password_bytes, salt)
+    password_string = password_hash.decode('utf-8')
+    return password_string
