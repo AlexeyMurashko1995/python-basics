4797eb61960d9290902964d29cdbcf38e417777a
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-21 00:07:34 +0200

    feat: implement password verification utility function

--- advanced_topics/app_practice.py
+++ advanced_topics/app_practice.py
@@ -11,3 +11,10 @@ def get_hash_password(password: str):
     hash_bytes = bcrypt.hashpw(password_bytes, salt)
     hash_string = hash_bytes.decode('utf-8')
     return hash_string
+
+
+def verify_password(hash_string: str, password: str):
+    password_bytes = password.encode('utf-8')
+    hash_bytes = hash_string.encode('utf-8')
+    comparison = bcrypt.checkpw(password_bytes, hash_bytes)
+    return comparison
