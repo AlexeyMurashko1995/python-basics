d38802f21b858c46d2c7a349fd9382920f757bcc
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-20 23:56:21 +0200

    feat: add password hashing utility function

--- advanced_topics/app_practice.py
+++ advanced_topics/app_practice.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+import bcrypt
 
 
 app = FastAPI()
 
+
+def get_hash_password(password: str):
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash_bytes = bcrypt.hashpw(password_bytes, salt)
+    hash_string = hash_bytes.decode('utf-8')
+    return hash_string
