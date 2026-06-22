ef9ac6d07904aa48ed98da8d0d1c7b16e7546437
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-22 21:17:04 +0200

    feat: implement database session generator using context manager

--- advanced_topics/app/database.py
+++ advanced_topics/app/database.py
@@ -1,8 +1,13 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine, SQLModel, Session
 
 
 filename = 'username.db'
 url = f'sqlite:///{filename}'
 
 engine = create_engine(url)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
 
