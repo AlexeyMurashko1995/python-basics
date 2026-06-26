b944e415ae0d012728a1b010294c0af75aaaebcb
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-26 21:34:46 +0200

    feat: add role field to User model with default customer value

--- advanced_topics/sandbox.py
+++ advanced_topics/sandbox.py
@@ -12,6 +12,7 @@ class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str = Field(unique=True)
     hashed_password: str
+    role: str | None = Field(default='customer')
 
 class UserCreate(BaseModel):
     username: str
