012bdc5f88b0edb26114ba468fa948d1ad478cd2
AlexMurashko <alexeymurashko1995@gmail.com>
2026-06-30 19:55:01 +0200

    feat(models): add Product table and schemas

---
+++ advanced_topics/market_project/app/models/product.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field
+from pydantic import BaseModel
+
+class Product(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    title: str = Field(unique=True)
+    price: int
+
+
+class ProductCreate(BaseModel):
+    title: str
+    price: int
