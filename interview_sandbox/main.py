68ac385a6cff2119e8c37492b8bb09f1013ed7e4
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-30 22:21:43 +0200

    feat(step-1.3): run tasks concurrently with asyncio gather

--- interview_sandbox/main.py
+++ interview_sandbox/main.py
@@ -3,14 +3,12 @@ import asyncio
 async def simple_task():
     print("Starting work...")
     await asyncio.sleep(2)
-    result = "Finishing..."
-    return result
+    print("Finishing")
 
 
 async def main():
-    res1 = await simple_task()
-    res2 = await simple_task()
-    print(res1, res2)
+    result = await asyncio.gather(simple_task(), simple_task())
+    print(result)
 
 
 asyncio.run(main())
