8d6e570cdfe16afb640b64db83201101cd7ba0d5
AlexMurashko <alexeymurashko1995@gmail.com>
2026-08-31 21:12:55 +0200

    feat: add asyncio.gather error handling with return_exceptions

--- interview_sandbox/main.py
+++ interview_sandbox/main.py
@@ -1,14 +1,21 @@
 import asyncio
 
-async def simple_task():
-    print("Starting work...")
-    await asyncio.sleep(2)
-    print("Finishing")
+
+async def get_delay(id_source: int, is_error: bool):
+    print(f"Starting {id_source} id")
+    await asyncio.sleep(1)
+    if not is_error:
+        return f"Finishing {id_source} id"
+    raise ValueError("Error")
 
 
 async def main():
-    result = await asyncio.gather(simple_task(), simple_task())
-    print(result)
+    result = await asyncio.gather(get_delay(1, False), get_delay(2, True), get_delay(3, False), return_exceptions=True)
+    for item in result:
+        if isinstance(item, Exception):
+            print(f"Failed: {item}")
+        else:
+            print(f"Success: {item}")
 
 
 asyncio.run(main())
