0ef01dd27619e2c07b5ed75fa0fcb8fdd4c442c7
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-01 21:38:53 +0200

    feat: offload synchronous blocking task to thread using asyncio.to_thread

--- interview_sandbox/async_tasks.py
+++ interview_sandbox/async_tasks.py
@@ -1,21 +1,38 @@
 import asyncio
+import time
 
-async def process_payload(payload_id: int, delay: int):
-    print(f"Starting: id {payload_id}")
-    await asyncio.sleep(delay)
-    if payload_id % 2 == 0:
-        raise ValueError(f"Error: id {payload_id}")
-    return f"Success: id {payload_id}"
+# async def process_payload(payload_id: int, delay: int):
+#     print(f"Starting: id {payload_id}")
+#     await asyncio.sleep(delay)
+#     if payload_id % 2 == 0:
+#         raise ValueError(f"Error: id {payload_id}")
+#     return f"Success: id {payload_id}"
+
+
+# async def main():
+#     result = await asyncio.gather(process_payload(1, 2), process_payload(2, 2), process_payload(3,3), process_payload(4,1), return_exceptions=True)
+#     for item in result:
+#         final = isinstance(item, Exception)
+#         if final:
+#             print(f"Error: {item}")
+#         else:
+#             print(f"Success: {item}")
+
+
+# asyncio.run(main())
+
+def sync_heavy_task():
+    print("Sync task started")
+    time.sleep(2)
+    print("Sync task finished")
+
+
+async def fast_async_task():
+    await asyncio.sleep(1)
+    print("Fast task .....")
 
 
 async def main():
-    result = await asyncio.gather(process_payload(1, 2), process_payload(2, 2), process_payload(3,3), process_payload(4,1), return_exceptions=True)
-    for item in result:
-        final = isinstance(item, Exception)
-        if final:
-            print(f"Error: {item}")
-        else:
-            print(f"Success: {item}")
-
+    result = await asyncio.gather(asyncio.to_thread(sync_heavy_task), fast_async_task())
 
 asyncio.run(main())
