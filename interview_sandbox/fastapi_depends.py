7c30ff88d15e4a47a419d7df0db069065cc24912
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-02 21:25:50 +0200

    feat: simulate db session lifecycle using async generator and try-finally

---
+++ interview_sandbox/fastapi_depends.py
@@ -0,0 +1,15 @@
+import asyncio
+
+async def get_db():
+    print("[DB]: Connection opened")
+    try:
+        yield "AsyncSession_Object"
+    finally:
+        print("[DB]: Connection closed")
+
+
+async def main():
+    async for session in get_db():
+        print(f"Our session: {session}")
+
+asyncio.run(main())
