e5bc1072171565afe025685336bc2e3b75a0bac4
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-04 22:09:56 +0200

    feat: add get_db session generator for database dependency injection

--- interview_sandbox/fastapi_practice_3.py
+++ interview_sandbox/fastapi_practice_3.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
 from fastapi import FastAPI
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from pydantic import BaseModel, ConfigDict
+
 
 engine = create_async_engine("sqlite+aiosqlite:///:memory:")
 
@@ -16,3 +18,20 @@ class PaymentDB(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     account_id: Mapped[int]
     amount: Mapped[float]
+
+
+class PaymentCreate(BaseModel):
+    account_id: int
+    amount: float
+
+
+class PaymentResponse(BaseModel):
+    id: int
+    account_id: int
+    amount: float
+    model_config = ConfigDict(from_attributes=True)
+
+
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
