dae52b703d5b2470f421d5a1ee59d2cb1071d3ab
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-06 18:52:03 +0200

    feat: add lifespan and get_db session generator

--- interview_sandbox/fastapi_practice_5.py
+++ interview_sandbox/fastapi_practice_5.py
@@ -1,10 +1,8 @@
+from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from pydantic import BaseModel, ConfigDict
-
-engine = create_async_engine("sqlite+aiosqlite:///:memory:")
-
-async_session_factory = async_sessionmaker(bind=engine, expire_on_commit=False)
+from contextlib import asynccontextmanager
 
 
 class Base(DeclarativeBase):
@@ -29,3 +27,24 @@ class OrderResponse(BaseModel):
     total_price: float
 
     model_config = ConfigDict(from_attributes=True)
+
+
+engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+
+async_session_factory = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
+
+
