9a3130e45e2cefdb6100d3321b2c3de4974f7af6
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-09 22:02:08 +0200

    feat: add get_author_with_books function with selectinload

--- interview_sandbox/fastapi_practice_10.py
+++ interview_sandbox/fastapi_practice_10.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, selectinload
+from sqlalchemy import ForeignKey, select
 
 engine = create_async_engine("sqlite+aiosqlite:///:memory:")
 
@@ -24,3 +24,15 @@ class Book(Base):
     title: Mapped[str]
     author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"))
     author: Mapped[Author] = relationship(back_populates="books")
+
+
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
+
+
+async def get_author_with_books(author_id: int, session: AsyncSession):
+    query = select(Author).where(Author.id==author_id).options(selectinload(Author.books))
+    result = await session.execute(query)
+    final = result.scalar_one_or_none()
+    return final
