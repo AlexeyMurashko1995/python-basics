8c6256df7cc3616be723a084113129bed1cbf011
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-09 22:43:20 +0200

    feat: complete loading strategies practice with selectinload and joinedload

--- interview_sandbox/fastapi_practice_10.py
+++ interview_sandbox/fastapi_practice_10.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship, selectinload, joinedload
 from sqlalchemy import ForeignKey, select
+from fastapi import Depends
 
 engine = create_async_engine("sqlite+aiosqlite:///:memory:")
 
@@ -42,3 +43,37 @@ async def get_books_with_authors(session: AsyncSession) -> list[Book]:
     query = select(Book).options(joinedload(Book.author))
     result = await session.execute(query)
     return result.scalars().all()
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    async with async_session_factory() as session:
+        author_1 = Author(name="Alex")
+        author_2 = Author(name="Ivan")
+        author_3 = Author(name="Oleg")
+        book_1 = Book(title="Book_1", author=author_1)
+        book_2 = Book(title="Book_2", author=author_2)
+        book_3 = Book(title="Book_3", author=author_3)
+        session.add_all([author_1, author_2, author_3, book_1, book_2, book_3])
+        await session.commit()
+
+
+async def main():
+    start = await init_db()
+    async with async_session_factory() as session:
+        author = await get_author_with_books(1, session=session)
+        if author:
+            print(f"Author: {author.name}")
+            for book in author.books:
+                print(f"Book: {book.title}")
+
+        books = await get_books_with_authors(session=session)
+        for book in books:
+            print(f" Book: {book.title} | Author: {book.author.name}")
+
+
+if __name__== "__main__":
+    import asyncio
+    asyncio.run(main())
+
