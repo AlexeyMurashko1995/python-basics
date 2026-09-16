ebbce96a58a01f28b0514d330e672eafd7b096a3
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-16 21:09:54 +0200

    feat: implement cache-aside invalidation pattern

--- interview_sandbox/redis_practice_10.py
+++ interview_sandbox/redis_practice_10.py
@@ -14,3 +14,19 @@ async def get_product_price(product_id: str):
     product_price = SIMULATED_DB[product_id]
     SIMULATED_REDIS[product_id] = product_price
     return product_price
+
+
+async def update_product_price(product_id: str, new_price: int):
+    SIMULATED_DB[product_id] = new_price
+    SIMULATED_REDIS.pop(product_id, None)
+    print("DB UPDATE & CACHE INVALIDATED")
+
+
+async def main():
+    await get_product_price("product_1")
+    await get_product_price("product_1")
+    await update_product_price("product_1", 150)
+    await get_product_price("product_1")
+
+
+asyncio.run(main())
