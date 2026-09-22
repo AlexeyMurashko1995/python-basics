88ad755f510ab635724799b2024e2710ec834919
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-22 21:59:30 +0200

    test: add check_age logic and validation tests

--- interview_sandbox/pytest_practice/math_utils.py
+++ interview_sandbox/pytest_practice/math_utils.py
@@ -1,3 +1,9 @@
 def divide(a: int, b: int):
     return a / b
 
+
+def check_age(age: int):
+    if age >= 18:
+        return "Adult"
+    elif age < 0:
+        raise ValueError("Invalid error")
--- interview_sandbox/pytest_practice/test_math.py
+++ interview_sandbox/pytest_practice/test_math.py
@@ -1,10 +1,20 @@
 import pytest
-from math_utils import divide
+from math_utils import divide, check_age
 
 
 def test_divide_success():
     assert divide(10, 2) == 5
 
+
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         divide(10, 0)
+
+
+def test_check_success():
+    assert check_age(20) == "Adult"
+
+
+def test_age_invalid():
+    with pytest.raises(ValueError):
+        check_age(-2)
