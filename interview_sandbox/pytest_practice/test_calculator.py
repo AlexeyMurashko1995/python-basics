07028e6a1f8e93162ccf503fdf649c58c730a316
AlexMurashko <alexeymurashko1995@gmail.com>
2026-09-24 20:55:07 +0200

    refactor: parametrize divide tests with exception handling

--- interview_sandbox/pytest_practice/test_calculator.py
+++ interview_sandbox/pytest_practice/test_calculator.py
@@ -2,10 +2,23 @@ import pytest
 from calculator import divide
 
 
-def test_divide_success():
-    assert divide(10, 2) == 5
+# def test_divide_success():
+#     assert divide(10, 2) == 5
 
 
-def test_divide_error():
-    with pytest.raises(ZeroDivisionError):
-        divide(10, 0)
+# def test_divide_error():
+#     with pytest.raises(ZeroDivisionError):
+#         divide(10, 0)
+
+
+@pytest.mark.parametrize("number_1, number_2, expected", [
+    (10, 2, 5),
+    (12, 3, 4),
+    (20, 0, ZeroDivisionError)
+])
+def test_divide_params(number_1, number_2, expected):
+    if expected == ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            divide(number_1, number_2)
+    else:
+        assert divide(number_1, number_2) == expected
